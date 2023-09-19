440232946e72831a5e13062ddd96955741272ad9
LilyTheOtter <96593130+LilyTheOtter@users.noreply.github.com>
2023-09-19 09:52:56 +0200

    Created overlay and added css for it

--- src/index.php
+++ src/index.php
@@ -14,7 +14,7 @@
     <div class=wrapper>
         <div class=desktop>
             <div class="apps">
-                <div class="icon">
+                <div class="icon" id="aboutmeIcon">
                     <img src="./images/application-pdf.svg" alt="PDF Icon">
                     <p>About me.pdf</p>
                 </div>
@@ -22,29 +22,17 @@
                     <img src="./images/application-pdf.svg" alt="PDF Icon">
                     <p>Projects summary.pdf</p>
                 </div>
-                <div class="icon">
-                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                    <p>Projects summary.pdf</p>
-                </div>
-                <div class="icon">
-                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                    <p>Projects summary.pdf</p>
-                </div>
-                <div class="icon">
-                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                    <p>Projects summary.pdf</p>
-                </div>
-                <div class="icon">
-                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                    <p>Projects summary.pdf</p>
-                </div>
-                <div class="icon">
-                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                    <p>Projects summary.pdf</p>
-                </div>
-                <div class="icon">
-                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                    <p>Projects summary.pdf</p>
+                <div class="aboutme-overlay" id="overlay">
+                    <div class="aboutme-overlay-window">
+                        <div class="aboutme-overlay-content-topbar">
+                            <button class="closeButton" id="closeOverlay"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></button>
+                        </div>
+                        <h2>About me</h2>
+                        <div class="aboutme-overlay-content">
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                            <img src="images/personal-picture.jpg" alt="Picture of myself">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/javascript.js
+++ src/javascript.js
@@ -11,8 +11,10 @@ function dateTime() {
   var day =
     date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
   // Get current time in format mm:ss
-  var time = 
-    date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0");
+  var time =
+    date.getHours().toString().padStart(2, "0") +
+    ":" +
+    date.getMinutes().toString().padStart(2, "0");
   // Inserts date and time into html document
   document.getElementById("display_date").innerHTML = day;
   document.getElementById("display_time").innerHTML = time;
@@ -20,3 +22,43 @@ function dateTime() {
   refresh_dateTime();
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the overlay and close button elements
+  const overlay = document.getElementById("overlay");
+  const closeOverlayButton = document.getElementById("closeOverlay");
+  const overlayTaskbar = document.querySelector('.aboutme-overlay-content-topbar');
+
+  let isDragging = false;
+  let offsetX, offsetY;
+
+  // Get the icon elements
+  const aboutmeIcon = document.getElementById("aboutmeIcon");
+  // Add click event listener to open the overlay
+  aboutmeIcon.addEventListener("click", function () {
+    overlay.style.display = "flex";
+  });
+
+  // Add click event listener to close the overlay
+  closeOverlayButton.addEventListener("click", function () {
+    overlay.style.display = "none";
+  });
+
+  document.addEventListener("mousemove", function (e) {
+    if (isDragging) {
+      overlay.style.left = e.clientX - offsetX + "px";
+      overlay.style.top = e.clientY - offsetY + "px";
+    }
+  });
+
+  document.addEventListener("mouseup", function () {
+    overlayTaskbar.style.removeProperty("cursor")
+    isDragging = false;
+  });
+
+  overlayTaskbar.addEventListener("mousedown", function (e) {
+    overlayTaskbar.style.cursor = "grabbing"
+    isDragging = true;
+    offsetX = e.clientX - overlay.offsetLeft;
+    offsetY = e.clientY - overlay.offsetTop;
+  });
+});
