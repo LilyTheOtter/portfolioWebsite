c6813ac9fb103026f1ccc8024d9e4e7ac47221c8
LilyTheOtter <96593130+LilyTheOtter@users.noreply.github.com>
2023-09-25 11:39:53 +0200

    Clippy works!

--- src/index.php
+++ src/index.php
@@ -32,6 +32,16 @@
                 </div>
                 <div>
 
+                    <div class="info-overlay" id="overlay4">
+                        <div id="closeOverlay4">
+                            <div class="infoText">
+                                <h3>Welcome to my website</h3>
+                                <p>To learn more about Lily you can open the pdf's and explorer by clicking them.</p>
+                                <p>To close this info popup, click on me!</p>
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="pdf-overlay" id="overlay1">
                         <div class="pdf-overlay-window">
                             <div class="overlay-content-topbar">
@@ -90,13 +100,6 @@
                         </div>
                     </div>
 
-                    
-                    <div class="info-overlay" id="overlay4">
-                        <div class="overlay-content-topbar-closebutton">
-                            <button class="closeButton" id="closeOverlay4"><img src="./images/cross.svg" alt="Close button"></button>
-                        </div>
-                        <img src="./images/clippy.png" alt="Image of clippy">
-                    </div>
                     <div class="pdf-overlay" id="overlay5">
                         <div class="pdf-overlay-window">
                             <div class="overlay-content-topbar">
@@ -107,14 +110,13 @@
                                     <button class="closeButton" id="closeOverlay5"><img src="./images/cross.svg" alt="Close button"></button>
                                 </div>
                             </div>
-                            <h2>Test</h2>
+                            <h2>About me</h2>
                             <div class="pdf-overlay-content">
-                                <p>test test</p>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                                <img src="images/personal-picture.jpg" alt="Picture of myself">
                             </div>
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
