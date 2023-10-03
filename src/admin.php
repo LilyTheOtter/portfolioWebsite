89c25dc9c98a529f7333ea3c47c4fa9e18b73d6a
LilyTheOtter <96593130+LilyTheOtter@users.noreply.github.com>
2023-10-03 09:54:29 +0200

    added check for login status and a WIP navbar

--- src/admin.php
+++ src/admin.php
@@ -1,5 +1,30 @@
 <?php
 session_start();
+if (!isset($_SESSION['loggedin'])) {
+    header("location: login");
+    exit;
+};
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="content">
+        <nav class="navbar">
+            <a href="../admin/newitem" class="nav-button"><p>hi</p></i></a>
+            <a href="../admin/main" class="nav-button"><i class="fa-duotone fa-house fa-2xl"></i></a>
+            <a href="../admin/logout" class="nav-button"><i class="fa-duotone fa-right-from-bracket fa-2xl"></i></a>
+        </nav>
+
+    </div>
+</body>
+
+</html>
