bd565a7f1a70494672e5423e969cd5ae85053a02
LilyTheOtter <96593130+LilyTheOtter@users.noreply.github.com>
2023-10-24 11:32:57 +0200

    added table with contents to admin page

--- src/admin.php
+++ src/admin.php
@@ -23,6 +23,52 @@ if (!isset($_SESSION['loggedin'])) {
             <a href="./main" class="nav-button" title="Add new content"><img src="./images/add-file.svg" alt="Add file icon"></a>
             <a href="./logout" class="nav-button" title="Log out"><img src="./images/logout.svg" alt="Logout icon"></a>
         </nav>
+        <h1>Admin page</h1>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Type</th>
+                <th>Inside</th>
+                <th>Location</th>
+                <th>Filename</th>
+                <th>Actions</th>
+            </tr>
+            <tbody class="tablebody">
+                <?php
+                require_once "backend/connection.php";
+
+                $stmt = $con->prepare("SELECT `id`, `type`, `inside`, `location`, `filename`, `disabled` FROM `content` WHERE NOT `type` = 'desktop'");
+                $stmt->execute();
+                $result = $stmt->get_result();
+                while ($row = $result->fetch_assoc()) {
+                    $id = $row["id"];
+                    $type = $row["type"];
+                    $inside = $row["inside"];
+                    $location = $row["location"];
+                    $filename = $row["filename"];
+                    $disabled = $row["disabled"];
+                    echo '<tr><td>' .
+                        $id . '</td><td>' .
+                        $type . '</td><td>' .
+                        $inside . '</td><td>' .
+                        $location . '</td><td>' .
+                        $filename . '</td>
+                    <td>
+                        <a href="disable?id=' . $id . '" title="Enable/disable" data-toggle="tooltip">';
+                    if (!$disabled) {
+                        echo '<img src="./images/eye.svg" alt="Eye Icon">';
+                    } else {
+                        echo '<img src="./images/eye-slash.svg" alt="Eye with slash Icon"';
+                    };
+                    echo '</span></a>
+                        <a href="edit?id=' . $id . '" title="Update Record" data-toggle="tooltip"><img src="./images/pencil.svg" alt="Pencil Icon"></span></a>'
+                        . '</span></a>
+                        <a href="delete?id=' . $id . '" title="Update Record" data-toggle="tooltip"><img src="./images/trash.svg" alt="Trash Icon"></span></a>' .
+                    '</td></tr>';
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
 </body>
 
--- src/index.php
+++ src/index.php
@@ -9,16 +9,6 @@
     <title>Document</title>
     <script defer type="text/javascript" src="javascript.js"></script>
 </head>
-<!--
-    Mogelijke manier om data dynamich in te laden via db:
-    - Sla de documenten op met pad/filename waarbij mogelijk / de desktop is. Zie gemaakte schets op tablet
-    - Laad dit in zoals hieronder maar ipv gelijk echo maak vul een array. (database responce sorted by path)
-    - Elk entry zonder ander path wordt in de array opgeslagen
-    - elke keer dat de loop een ander file path tegen komt echoed die alle dingen uit de array en gaat die verder met een lege array.
-    - Dit door tot je door alles heen bent.
-
-
--->
 
 <body onload=dateTime();>
     <div class=wrapper>
@@ -64,6 +54,7 @@
                             break;
 
                         case 'explorer':
+
                             echo '<div class="icon" id="icon' . $id . '">
                             <img src="./images/system-file-manager.svg" alt="PDF Icon">
                             <p>' . $filename . '</p>
@@ -84,10 +75,12 @@
 
                             echo '</div>
                                     <div class="explorer-overlay-content">';
-                            $stmt = $con->prepare("SELECT `id`, `type`, `inside`, `location`, `filename`, `content` FROM `content` where `inside` = 'explorer'");
-                            $stmt->execute();
-                            $result = $stmt->get_result();
-                            while ($row = $result->fetch_assoc()) {
+                            $explorerId = ++$id;
+                            $statement = $con->prepare("SELECT `id`, `type`, `inside`, `location`, `filename`, `content` FROM `content` where `inside` = 'explorer' and `location` = ?");
+                            $statement->bind_param("i", $explorerId);
+                            $statement->execute();
+                            $results = $statement->get_result();
+                            while ($row = $results->fetch_assoc()) {
                                 $id = $row['id'];
                                 $id = --$id;
                                 $type = $row['type'];
@@ -152,172 +145,3 @@
 
 </html>
 
-<?php exit; ?>
-
-<body onload=dateTime();>
-    <div class=wrapper>
-        <div class=desktop>
-            <div class="apps">
-
-                <div class="icon" id="icon1">
-                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                    <p>About me.pdf</p>
-                </div>
-                <div class="icon" id="icon2">
-                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                    <p>Projects summary.pdf</p>
-                </div>
-                <div class="icon" id="icon3">
-                    <img src="./images/system-file-manager.svg" alt="PDF Icon">
-                    <p>All projects</p>
-                </div>
-                <div class="icon" id="icon4">
-                    <img src="./images/info.svg" alt="Info center Icon">
-                    <p>Info center</p>
-                </div>
-                <div>
-
-                    <div class="info-overlay" id="overlay4">
-                        <div class="overlay-content-topbar" style="display: none;"></div>
-                        <div id="closeOverlay4">
-                            <div class="infoText">
-                                <h3>Welcome to my website</h3>
-                                <p>To learn more about Lily you can open the pdf's and explorer by clicking them.</p>
-                                <p>To close this info popup, click on me!</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="pdf-overlay" id="overlay1">
-                        <div class="pdf-overlay-window">
-                            <div class="overlay-content-topbar">
-                                <div class="overlay-content-topbar-icon">
-                                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                                </div>
-                                <div class="overlay-content-topbar-closebutton">
-                                    <button class="closeButton" id="closeOverlay1"><img src="./images/cross.svg" alt="Close button"></button>
-                                </div>
-                            </div>
-                            <h2>About me</h2>
-                            <div class="pdf-overlay-content">
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-                                <img src="images/personal-picture.jpg" alt="Picture of myself">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="pdf-overlay" id="overlay2">
-                        <div class="pdf-overlay-window">
-                            <div class="overlay-content-topbar">
-                                <div class="overlay-content-topbar-icon">
-                                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                                </div>
-                                <button class="closeButton" id="closeOverlay2"><img src="./images/cross.svg" alt="Close button"></button>
-                            </div>
-                            <h2>My projects</h2>
-                            <div class="pdf-overlay-content">
-                                <p>This website</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="explorer-overlay" id="overlay3">
-                        <div class="explorer-overlay-window">
-                            <div class="explorer-overlay-grid">
-                                <div class="explorer-overlay-ribbon">
-                                    <div class="overlay-content-topbar">
-                                        <div class="overlay-content-topbar-icon">
-                                            <img src="./images/system-file-manager.svg" alt="PDF Icon">
-                                        </div>
-                                        <button class="closeButton" id="closeOverlay3"><img src="./images/cross.svg" alt="Close button"></button>
-                                    </div>
-                                </div>
-                                <div class="explorer-overlay-navigationPane">
-
-                                </div>
-                                <div class="explorer-overlay-content">
-                                    <div class="icon" id="icon5">
-                                        <img src="./images/application-pdf.svg" alt="PDF Icon">
-                                        <p>Test.pdf</p>
-                                    </div>
-                                    <div class="icon" id="icon6">
-                                        <img src="./images/application-pdf.svg" alt="PDF Icon">
-                                        <p>Test.pdf</p>
-                                    </div>
-                                    <div class="icon" id="icon7">
-                                        <img src="./images/info.svg" alt="Info center Icon">
-                                        <p>Info center</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pdf-overlay" id="overlay5">
-                        <div class="pdf-overlay-window">
-                            <div class="overlay-content-topbar">
-                                <div class="overlay-content-topbar-icon">
-                                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                                </div>
-                                <button class="closeButton" id="closeOverlay5"><img src="./images/cross.svg" alt="Close button"></button>
-                            </div>
-                            <h2>My projects</h2>
-                            <div class="pdf-overlay-content">
-                                <p>This website</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="pdf-overlay" id="overlay6">
-                        <div class="pdf-overlay-window">
-                            <div class="overlay-content-topbar">
-                                <div class="overlay-content-topbar-icon">
-                                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                                </div>
-                                <div class="overlay-content-topbar-closebutton">
-                                    <button class="closeButton" id="closeOverlay6"><img src="./images/cross.svg" alt="Close button"></button>
-                                </div>
-                            </div>
-                            <h2>About me</h2>
-                            <div class="pdf-overlay-content">
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-                                <img src="images/personal-picture.jpg" alt="Picture of myself">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pdf-overlay" id="overlay7">
-                        <div class="pdf-overlay-window">
-                            <div class="overlay-content-topbar">
-                                <div class="overlay-content-topbar-icon">
-                                    <img src="./images/application-pdf.svg" alt="PDF Icon">
-                                </div>
-                                <div class="overlay-content-topbar-closebutton">
-                                    <button class="closeButton" id="closeOverlay7"><img src="./images/cross.svg" alt="Close button"></button>
-                                </div>
-                            </div>
-                            <h2>About me</h2>
-                            <div class="pdf-overlay-content">
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-                                <img src="images/personal-picture.jpg" alt="Picture of myself">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class=taskbar>
-            <div class="taskbarLeft">
-                <div class=startMenuButton>
-                    <img src="./images/wip-icon.png" alt="Start menu button">
-                </div>
-            </div>
-            <div class="taskbarRight">
-                <div class=time>
-                    <span id='display_time'></span><br>
-                    <span id='display_date'></span>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-
-</html>
