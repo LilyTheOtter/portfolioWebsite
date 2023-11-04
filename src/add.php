5d9560d66e19f0b3a6d5efc71f297285311389bf
LilyTheOtter <96593130+LilyTheOtter@users.noreply.github.com>
2023-11-04 21:41:16 +0100

    ⚰️ Page does not exist

--- src/add.php
+++ src/add.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['loggedin'])) {
 <body>
     <div class="content">
         <nav class="navbar">
-            <a href="./newitem" class="nav-button" title="Go to users"><img src="./images/people.svg" alt="People icon"></a>
+            <a href="./" class="nav-button" title="Go to users"><img src="./images/people.svg" alt="People icon"></a>
             <a href="./main" class="nav-button" title="Add new content"><img src="./images/add-file.svg" alt="Add file icon"></a>
             <a href="./logout" class="nav-button" title="Log out"><img src="./images/logout.svg" alt="Logout icon"></a>
         </nav>
