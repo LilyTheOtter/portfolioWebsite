00ca1084a93e82ad2d3fb9d89b7c042b6dce0f16
LilyTheOtter <96593130+LilyTheOtter@users.noreply.github.com>
2023-11-04 21:10:02 +0100

    🐛 Fixed database creation command

--- databaseInitScript/InitScript.sql
+++ databaseInitScript/InitScript.sql
@@ -1,4 +1,4 @@
-create schema portfolioWebsite charset set 'utf8mb4_0900_as_cs';
+CREATE DATABASE portfolioWebsite CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_as_cs'; 
 use portfolioWebsite;
 CREATE TABLE `users` (
     `id` int NOT NULL AUTO_INCREMENT,
