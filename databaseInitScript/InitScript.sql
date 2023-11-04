04fd9d162dcc617279811c95ba90016a39d43dd8
LilyTheOtter <96593130+LilyTheOtter@users.noreply.github.com>
2023-11-04 21:25:46 +0100

    🐛 desktop is now created on startup

--- databaseInitScript/InitScript.sql
+++ databaseInitScript/InitScript.sql
@@ -1,23 +1,34 @@
-CREATE DATABASE portfolioWebsite CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_as_cs'; 
+CREATE DATABASE portfolioWebsite CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_as_cs';
 use portfolioWebsite;
+
 CREATE TABLE `users` (
     `id` int NOT NULL AUTO_INCREMENT,
     `username` varchar(255) NOT NULL,
-    `permissiongroup` ENUM("Admin","Editor"),
+    `permissiongroup` ENUM("Admin", "Editor"),
     `disabled` BOOLEAN,
     `activationcode` varchar(255),
     `password` varchar(64),
     `salt` varchar(64),
     PRIMARY KEY(id)
-    );
+);
 
 CREATE TABLE `content` (
-	`id` int NOT NULL AUTO_INCREMENT,
-	`type`varchar(32),
-	`inside` varchar(32),
-	`location` varchar(32),
-	`filename` varchar(64),
-	`disabled` BOOLEAN,
-	`content` LONGTEXT,
-	PRIMARY KEY(id)
-    );
+    `id` int NOT NULL AUTO_INCREMENT,
+    `type` varchar(32),
+    `inside` varchar(32),
+    `location` varchar(32),
+    `filename` varchar(64),
+    `disabled` BOOLEAN,
+    `content` LONGTEXT,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO `content` (
+        `type`,
+        `inside`,
+        `location`,
+        `filename`,
+        `disabled`,
+        `content`
+    )
+VALUES ('desktop', 'root', 'root', NULL, '0', NULL);
